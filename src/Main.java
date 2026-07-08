575e6df8e032811ea12c81d19ac37feb97da5a35
LeDacHuy203 <ldh2k3@gmail.com>
2026-07-08 07:50:19 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle(3, 4);
-        Rectangle r2 = new Rectangle(4.5, 3.5);
+        Rectangle r2 = new Rectangle(5, 2);
         Rectangle r3 = new Rectangle(4.5, 3.5);
 
 
