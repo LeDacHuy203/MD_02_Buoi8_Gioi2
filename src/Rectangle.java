8280d0ccb2823b11b2ff759556114c35e4a78904
LeDacHuy203 <ldh2k3@gmail.com>
2026-07-08 07:49:32 +0700

    update

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -11,17 +11,11 @@ public class Rectangle {
         return height;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
 
     public double getWidth() {
         return width;
     }
 
-    public void setWidth(double width) {
-        this.width = width;
-    }
 
     public double getArea() {
         return this.width * this.height;
